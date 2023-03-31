324265595aae5bd3cec8759c51d0d8918ed7bf93
Nasrin F. Asadli <nasrinfa@code.local>
2023-03-31 17:30:35 +0400

    added Unique-Number.cs

---
+++ Unique-Number.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = { 1, 1, 2, 2, 3, 4, 5, 5, 7, 8, 8, 9, 9 };
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    bool x = true;
+    for (int r = 0; r < numbers.Length; r++)
+    {
+        if (r != i && numbers[r] == numbers[i])
+        {
+            x = false;
+            break;
+        }
+    }
+
+    if (x == true)
+    {
+        Console.WriteLine(numbers[i]);
+    }
+}
