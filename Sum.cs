08f62629dcc1354b9935fb0e1b1717a2f4ac6086
Nasrin F. Asadli <nasrinfa@code.local>
2023-03-31 17:29:30 +0400

    added Sum.cs

---
+++ Sum.cs
@@ -0,0 +1,8 @@
+﻿ int[] numbers = {6, 5, 7, 18, 29, 36};
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+    sum += numbers[i];
+}
+Console.WriteLine("cem = " + sum);
