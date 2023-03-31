6a721bb2672b9341d7c84618383257e5717f4fb3
Nasrin F. Asadli <nasrinfa@code.local>
2023-03-31 17:27:35 +0400

    added Hello-World.cs

---
+++ Hello-World.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, World!");
+string sentence = "Hello, World!";
+int i = 0;
+int count = 0;
+while (i < sentence.Length)
+{
+    if (sentence[i] == ' ' || sentence[i] == ',' || sentence[i] == '!')
+    {
+        i++;
+    }
+
+    else
+    {
+        i++;
+        count++;
+    }
+}
+Console.WriteLine(count);
